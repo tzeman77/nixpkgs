ba8d3f255d2a916d25f9e3a7bac1be542678362f
Tomas Zeman <tzeman@volny.cz>
2013-09-05 15:05:57 +0200

    Added man pages

--- pkgs/tools/networking/ucspi-tcp/default.nix
+++ pkgs/tools/networking/ucspi-tcp/default.nix
@@ -1,7 +1,22 @@
 { stdenv, fetchurl, dietlibc }:
 
-stdenv.mkDerivation rec {
-  name = "ucspi-tcp-0.88";
+let
+  pkg = "ucspi-tcp";
+  ver = "0.88";
+in stdenv.mkDerivation rec {
+  name = "${pkg}-${ver}";
+
+  srcs = [
+    (fetchurl {
+      url = "http://cr.yp.to/ucspi-tcp/${pkg}-${ver}.tar.gz";
+      sha256 = "171yl9kfm8w7l17dfxild99mbf877a9k5zg8yysgb1j8nz51a1ja";
+    })
+    (fetchurl {
+      url = "http://smarden.org/pape/djb/manpages/${pkg}-${ver}-man.tar.gz";
+      sha256 = "1vq8nibyjq8cirif8iv2bpzp5jv0jpgjgfqa0cnbc8ipr9lxvc89";
+    })
+  ];
+  sourceRoot = "${pkg}-${ver}";
 
   buildInputs = [dietlibc];
 
@@ -18,10 +33,11 @@ stdenv.mkDerivation rec {
   dontStrip = true; # diet does not need stripping
   dontPatchELF = true; # we produce static binaries
 
-  src = fetchurl {
-    url = http://cr.yp.to/ucspi-tcp/ucspi-tcp-0.88.tar.gz;
-    sha256 = "171yl9kfm8w7l17dfxild99mbf877a9k5zg8yysgb1j8nz51a1ja";
-  };
+  # man pages
+  postInstall = ''
+    mkdir -p $out/share/man/man1
+    cp $NIX_BUILD_TOP/${pkg}-${ver}-man/*.1 $out/share/man/man1/
+  '';
 
   meta = {
     description = "Command-line tools for building TCP client-server applications";
