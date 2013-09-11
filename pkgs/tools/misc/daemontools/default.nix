895375226a1fd3c7366198e83142aedbe8175a1f
Tomas Zeman <tzeman@volny.cz>
2013-09-11 21:40:08 +0200

    daemontools: cleanup, man pages

--- pkgs/tools/misc/daemontools/default.nix
+++ pkgs/tools/misc/daemontools/default.nix
@@ -19,43 +19,28 @@ in stdenv.mkDerivation rec {
   ];
   sourceRoot = "admin/${pkg}-${ver}";
 
-  /*
-  ipv6patch = fetchurl {
-    url = "http://www.fefe.de/ucspi/${pkg}-${ver}-ipv6.diff19.bz2";
-    sha256 = "1kls3xysv3wwmikxychnbfv8vbnb0ih15005hhn4a56pj392r59m";
-  };
-
-  patches = ipv6patch;
-  */
-
   buildInputs = [dietlibc];
 
   configurePhase = ''
-    echo "diet gcc" > conf-cc
-    echo "diet gcc -s -static" > conf-ld
+    echo "diet gcc" > src/conf-cc
+    echo "diet gcc -s -static" > src/conf-ld
   '';
 
   buildPhase = "./package/compile";
 
   installPhase = ''
-    mkdir $out/bin
+    mkdir -p $out/bin
     for BIN in `cat package/commands`; do
       cp command/$BIN $out/bin
     done
+    mkdir -p $out/man/man8
+    cp $NIX_BUILD_TOP/${pkg}-man/*.8 $out/man/man8/
   '';
 
   allowedReferences = ["out"];
   dontPatchShebangs = true; # /bin/sh
   dontStrip = true; # diet does not need stripping
   dontPatchELF = true; # we produce static binaries
-
-  # man pages
-  /*
-  postInstall = ''
-    mkdir -p $out/man/man1
-    cp $NIX_BUILD_TOP/${pkg}-${ver}-man/*.1 $out/man/man1/
-  '';
-  */
 
   meta = {
     description = "A collection of tools for managing UNIX services";
